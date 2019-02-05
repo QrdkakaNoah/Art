54c2b8a84f5b187dfd1572de6c324ae639214af7
QrdkakaNoah <46796628+QrdkakaNoah@users.noreply.github.com>
2019-02-05 14:46:56 -0700

    Add files via upload
    Added input at the end to prevent the program from automatically closing.
    (???) I can't remember if this already had color or not so if it did then that is unchanged but if it didn't then I added color (???)

---
+++ Noah's python art project.py
@@ -0,0 +1,43 @@
+import turtle, random
+colors = ["red","green","blue","yellow"]
+
+turtle = turtle.Turtle()
+turtle.speed(0)
+def square():
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+square()
+def art():
+    for i in range(90):
+        turtle.left(1)
+        square()
+art()
+def passpartout():
+    for i in range(4):
+        art()
+passpartout()
+def passpartout2():
+    for i in range(4):
+        color = random.choice(colors)
+        turtle.color(color)
+        turtle.forward(200)
+        art()
+passpartout2()
+def passpartout3():
+    for i in range (4):
+        color = random.choice(colors)
+        turtle.color(color)
+        turtle.left(180)
+        turtle.forward(200)
+        art()
+passpartout3()
+input()
+
+        
+
